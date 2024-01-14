64ce52e0454e111f8f1a0a072e176beaee6172d7
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-01-14 11:48:50 -0300

    chore: add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 import { CategoryModule } from './category/category.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PersonModule, CategoryModule],
+  imports: [PersonModule, CategoryModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
