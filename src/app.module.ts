b80266e6ca82f23a0e06a987f8ffa02a604a5fe3
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-01-14 11:49:23 -0300

    chore: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PersonModule, CategoryModule, UserModule],
+  imports: [PersonModule, CategoryModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
