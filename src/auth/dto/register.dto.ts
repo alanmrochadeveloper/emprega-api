c54ad11c70076b66c43da9dcf312e756e6960fe5
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-02-24 15:58:21 -0300

    chore: add class-validator and class-transformer

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,7 +1,19 @@
-export interface RegisterDTO {
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class RegisterDTO {
+    @IsNotEmpty()
     first_name: string;
+
+    @IsNotEmpty()
     last_name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     password: string;
+
+    @IsNotEmpty()
     confirm_password: string;
 }
