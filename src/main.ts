79f28d20b51e624c1bbf89a5382494f5ee39744a
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-04-20 08:14:51 -0300

    chore: few tweaks

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
 import "reflect-metadata";
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  console.log(process.env.API_PORT);
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api")
-  app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } }))
-  app.enableCors({ credentials: true })
-  app.use(cookieParser())
-  await app.listen(3001);
+  app.setGlobalPrefix("api");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    })
+  );
+  app.enableCors({ credentials: true });
+  app.use(cookieParser());
+  await app.listen(process.env.API_PORT || 3001);
 }
 bootstrap();
