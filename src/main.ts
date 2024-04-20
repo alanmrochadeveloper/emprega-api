4c148c13301a7e5b58458c08e3aad273828cf7fa
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-04-20 20:34:22 -0300

    chore: add again httpOptions

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { ValidationPipe } from "@nestjs/common";
+import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
+import { readFileSync } from "fs";
 import "reflect-metadata";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  // const httpsOptions: HttpsOptions = {
-  //   key: readFileSync("../private.key"),
-  //   cert: readFileSync("../certificate.crt"),
-  // };
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions: HttpsOptions = {
+    key: readFileSync("../private.key"),
+    cert: readFileSync("../certificate.crt"),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
     new ValidationPipe({
