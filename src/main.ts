d465d7e930ba66d7d7745fcd71178be40dfa9139
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-04-20 18:52:50 -0300

    chore: remove https from main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { ValidationPipe } from "@nestjs/common";
-import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
-import { readFileSync } from "fs";
 import "reflect-metadata";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const httpsOptions: HttpsOptions = {
-    key: readFileSync("../private.key"),
-    cert: readFileSync("../certificate.crt"),
-  };
+  // const httpsOptions: HttpsOptions = {
+  //   key: readFileSync("../private.key"),
+  //   cert: readFileSync("../certificate.crt"),
+  // };
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.useGlobalPipes(
