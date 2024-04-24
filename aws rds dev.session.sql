93f5ee9fe6e3a1d87c703e02e52ee9174c337a78
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2024-04-23 21:56:27 -0300

    chore: add sql file to .gitignore

--- aws rds dev.session.sql
+++ aws rds dev.session.sql
@@ -2,3 +2,9 @@
 -- INSERT INTO admin_email (email) VALUES ('alanmrochadeveloper@gmail.com');
 
 -- SELECT * FROM admin_email
+
+-- select * from "user"
+
+-- select * from "person"
+
+-- delete from "person"
